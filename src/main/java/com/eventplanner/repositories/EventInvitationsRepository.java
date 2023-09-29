490fe97ca7a5ac7c29114e705372daa2a073950b
slayenger <sinyavskiym@mail.ru>
2023-09-29 23:16:38 +0300

    add swagger and fixed a typo

--- src/main/java/com/eventplanner/repositories/EventInvitationsRepository.java
+++ src/main/java/com/eventplanner/repositories/EventInvitationsRepository.java
@@ -1,8 +1,9 @@
 package com.eventplanner.repositories;
 
+import com.eventplanner.entities.EventInvitations;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
-public interface EventInvitationsRepository extends JpaRepository<EventInvitationsRepository, UUID> {
+public interface EventInvitationsRepository extends JpaRepository<EventInvitations, UUID> {
 }
